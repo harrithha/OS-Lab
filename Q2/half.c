09325c35e033b6e7340c8e2a5c0c22900e62fe14
harrithha <harrithha64@gmail.com>
2023-01-12 08:57:35 +0530

    Updates

---
+++ Q2/half.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int num = atoi(argv[argc-1]);
+    int pid = getpid();
+    int result = num/2;
+
+    printf("Half: Current process id: %d, Current result: %d\n", pid, result);
+    if(argc>2)
+    {
+        sprintf(argv[argc-1], "%d", result);
+        execvp(argv[1], argv + 1);
+    }
+    
+    return 0;
+}
